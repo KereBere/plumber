183837c439d4714868a0d0203efff3fed10dbb08
KereBere <alsydm091@gmail.com>
2021-10-04 15:15:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const hamButton = document.getElementsByClassName('ham-con')[0];
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    navbarLinks.classList.toggle('active')
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    navbarLinks.classList.toggle('active')
+    menuOpen = false;
+  }
+});
+
+  let slider = tns({
+    container: '.my-slider',
+    items: 1,
+    speed: 600,
+    controls: false,
+    mouseDrag: true,
+    autoplayHoverPause: false,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    autoplayButtonOutput: false,
+    nav: false
+    
+  });
+
+  const navShow = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    
+    burger.addEventListener('click', ()=>{
+      nav.classList.toggle('nav-active');
+    })
+  }
+
+  navShow();
